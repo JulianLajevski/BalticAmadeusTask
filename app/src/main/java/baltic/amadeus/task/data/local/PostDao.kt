03340a0dfe114ef1dd60a8b35e3495bafe858a3c
Julian Lajevski <julian.lajevski@gmail.com>
2021-06-12 23:40:57 +0300

    Added database and table name constants and created Dao interface.

---
+++ app/src/main/java/baltic/amadeus/task/data/local/PostDao.kt
@@ -0,0 +1,13 @@
+package baltic.amadeus.task.data.local
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import baltic.amadeus.task.data.entities.posts.Post
+
+@Dao
+interface PostDao {
+
+}
--- app/src/main/java/baltic/amadeus/task/utils/Constants.kt
+++ app/src/main/java/baltic/amadeus/task/utils/Constants.kt
@@ -4,5 +4,7 @@ class Constants {
     companion object{
         const val BASE_URL = "https://jsonplaceholder.typicode.com/"
         const val IMAGE_URL = "https://source.unsplash.com/collection/542909/?sig="
+        const val DATABASE_NAME = "posts_database"
+        const val POSTS_TABLE = "posts_table"
     }
 }
