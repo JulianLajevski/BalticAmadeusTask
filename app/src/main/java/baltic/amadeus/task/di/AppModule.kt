848c7afaee09d55ab2f790339eb73379b1f531af
Julian Lajevski <julian.lajevski@gmail.com>
2021-06-13 15:38:42 +0300

    Created app module class where dependency injection is used.

---
+++ app/src/main/java/baltic/amadeus/task/di/AppModule.kt
@@ -0,0 +1,50 @@
+package baltic.amadeus.task.di
+
+import android.content.Context
+import baltic.amadeus.task.data.local.AppDatabase
+import baltic.amadeus.task.data.local.PostDao
+import baltic.amadeus.task.data.remote.Api
+import baltic.amadeus.task.data.remote.ApiRemoteDataSource
+import baltic.amadeus.task.data.repository.PostRepository
+import baltic.amadeus.task.utils.Constants.Companion.BASE_URL
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import dagger.Provides
+import dagger.hilt.android.qualifiers.ApplicationContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(gson: Gson) : Retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build()
+
+    @Provides
+    fun provideGson(): Gson = GsonBuilder().create()
+
+    @Provides
+    fun provideCharacterService(retrofit: Retrofit): Api = retrofit.create(Api::class.java)
+
+    @Singleton
+    @Provides
+    fun provideCharacterRemoteDataSource(api: Api) = ApiRemoteDataSource(api)
+
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext appContext: Context) = AppDatabase.getDatabase(appContext)
+
+    @Singleton
+    @Provides
+    fun provideCharacterDao(db: AppDatabase) = db.postDao()
+
+    @Singleton
+    @Provides
+    fun provideRepository(remoteDataSource: ApiRemoteDataSource,
+                          localDataSource: PostDao) =
+        PostRepository(remoteDataSource, localDataSource)
+}
