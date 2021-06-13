b0bfc3aab33c24689c352b1dbb702e4277a65d54
Julian Lajevski <julian.lajevski@gmail.com>
2021-06-13 15:27:07 +0300

    Created Post repository.

---
+++ app/src/main/java/baltic/amadeus/task/data/repository/PostRepository.kt
@@ -0,0 +1,17 @@
+package baltic.amadeus.task.data.repository
+
+import baltic.amadeus.task.data.local.PostDao
+import baltic.amadeus.task.data.remote.ApiRemoteDataSource
+import baltic.amadeus.task.utils.performGetOperation
+import javax.inject.Inject
+
+class PostRepository @Inject constructor(
+    private val apiRemoteDataSource: ApiRemoteDataSource,
+    private val postDao: PostDao
+){
+    fun getPosts() = performGetOperation(
+        databaseQuery = {postDao.readAllData()},
+        networkCall = {apiRemoteDataSource.getPosts()},
+        saveCallResult = {postDao.insertData(it)}
+    )
+}
