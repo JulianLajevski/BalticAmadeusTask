42984f8bcea6d9dfa42c7ce4c31084092bad66ff
Julian Lajevski <julian.lajevski@gmail.com>
2021-06-14 01:10:13 +0300

    Added post view model.

---
+++ app/src/main/java/baltic/amadeus/task/ui/posts/PostViewModel.kt
@@ -0,0 +1,11 @@
+package baltic.amadeus.task.ui.posts
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+import baltic.amadeus.task.data.repository.PostRepository
+
+class PostViewModel @ViewModelInject constructor(
+    private val repository: PostRepository
+): ViewModel(){
+    val posts = repository.getPosts()
+}
