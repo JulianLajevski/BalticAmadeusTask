12106a1ba450422d3d556980165591a5b9a1d64a
Julian Lajevski <julian.lajevski@gmail.com>
2021-06-15 16:11:32 +0300

    Added new onPostClick method. That after click on recycler view item navigate to other fragment.

--- app/src/main/java/baltic/amadeus/task/utils/BindingAdapter.kt
+++ app/src/main/java/baltic/amadeus/task/utils/BindingAdapter.kt
@@ -1,4 +1,28 @@
 package baltic.amadeus.task.utils
 
+import android.util.Log
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.databinding.BindingAdapter
+import androidx.navigation.findNavController
+import baltic.amadeus.task.data.entities.posts.Post
+import baltic.amadeus.task.ui.posts.PostFragmentDirections
+
 class BindingAdapter {
+    companion object {
+
+        @BindingAdapter("onPostClick")
+        @JvmStatic
+        fun onPostClick(postRowLayout: ConstraintLayout, post: Post) {
+            postRowLayout.setOnClickListener {
+                try {
+                    val action =
+                        PostFragmentDirections.actionPostFragmentToPostDetailsFragment(post)
+
+                    postRowLayout.findNavController().navigate(action)
+                } catch (e: Exception) {
+                    Log.d("OnPostClick", e.message.toString())
+                }
+            }
+        }
+    }
 }
